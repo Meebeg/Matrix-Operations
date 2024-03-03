a413df6ae6223f6fd037ac84daa6ffc21d57aa1e
Пользователь <aaanonmhaaa@gmail.com>
2024-03-03 21:40:57 +0400

    last changes

--- Matrix_Operations.py
+++ Matrix_Operations.py
@@ -1,1 +1,41 @@
 
+
+# #1st Task
+# import random as rd
+# def generate_random_matrix(rows, cols):
+#     matrix = []
+#     for _ in range(rows):
+#         row = [rd.randint(1, 100) for _ in range(cols)]
+#         matrix.append(row)
+#     return matrix
+
+
+# rd_mat = generate_random_matrix(2, 4)
+# for row in rd_mat:
+#     print(row)
+
+# #2nd Task
+# def get_column_sum(matrix, index):
+#     col_sum= 0
+#     for row in matrix:
+#         col_sum += row[index]
+#     return col_sum
+# matrix = [
+#     [5, 6 ,8],
+#     [4, 5 ,6]
+#     ]
+# index = 0
+# col_sum = get_column_sum(matrix, index)
+# print(matrix, "The sum of column", index, "is:", col_sum)
+
+def get_row_average(matrix, index):
+    row_values = matrix[index]
+    row_average = sum(row_values) / len(row_values)
+    return row_average
+matrix = [
+    [5, 6 ,8],
+    [4, 5 ,6]
+    ]
+index = 0
+row_average = get_row_average(matrix, index)
+print(matrix, "The average of rows", index, "is:", row_average)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Matrix Operations
+Create a function called generate_random_matrix that takes two arguments: rows and cols. This function should generate a 2D matrix of random integers between 1 and 100, with rows rows and cols columns. Use the random module from the Python standard library to generate random numbers. The matrix should be returned as a list of lists.
+
+Create a function called get_column_sum that takes two arguments: a matrix (2D list) and a column index. This function should calculate and return the sum of all the values in the specified column of the matrix.
+
+Create a function called get_row_average that takes two arguments: a matrix (2D list) and a row index. This function should calculate and return the average (mean) of all the values in the specified row of the matrix.
